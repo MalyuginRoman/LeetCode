40291d82ca1a4699b3594a55612d0d81455ee3d0
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-17 21:59:03 +0500

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

---
+++ string/roman-to-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int count = s.size();
+        int result = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (s.at(i) == "I")
+                resilt +=1;
+        }
+    }
+};
