770321512666b730f30aa5d7b35bf1605ef8235e
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-17 22:09:25 +0500

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

--- math/roman-to-integer.cpp
+++ math/roman-to-integer.cpp
@@ -4,10 +4,28 @@ public:
         int count = s.size();
         int result = 0;
         char str1 = 'I';
+        char str2 = 'V';
+        char str3 = 'X';
+        char str4 = 'L';
+        char str5 = 'C';
+        char str6 = 'D';
+        char str7 = 'M';
         for (int i = 0; i < count; i++)
         {
             if (s.at(i) == str1)
                 result += 1;
+            if (s.at(i) == str2)
+                result += 5;
+            if (s.at(i) == str3)
+                result += 10;
+            if (s.at(i) == str4)
+                result += 50;
+            if (s.at(i) == str5)
+                result += 100;
+            if (s.at(i) == str6)
+                result += 500;
+            if (s.at(i) == str7)
+                result += 1000;
         }
         return result;
     }
