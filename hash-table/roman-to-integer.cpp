53b9e77cfba12f53b6f3f53158fa25c2590f0a62
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-17 22:06:08 +0500

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

--- hash-table/roman-to-integer.cpp
+++ hash-table/roman-to-integer.cpp
@@ -3,11 +3,11 @@ public:
     int romanToInt(string s) {
         int count = s.size();
         int result = 0;
-        const char* str1 = "I";
+        const char* str1 = I;
         for (int i = 0; i < count; i++)
         {
             if (&s.at(i) == str1)
-                result +=1;
+                result += 1;
         }
         return result;
     }
