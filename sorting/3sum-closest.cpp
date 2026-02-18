7c7fe63e3c224d97c5ad3febbc1526baadddd4d3
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-18 21:48:42 +0500

    LC-3sum-closest [Runtime: 1194 ms; Memory: 14.1 MB]

--- sorting/3sum-closest.cpp
+++ sorting/3sum-closest.cpp
@@ -8,33 +8,33 @@ public:
         int answer = (nums[0] + nums[1] + nums[2]);
         for (int i = 0; i < nums.size(); i++)
         {
-        for (int j = i + 1; j < nums.size(); j++)
-        {
-        for (int k = j + 1; k < nums.size(); k++)
-        {
-if ((nums[i] + nums[j] + nums[k]) == target)
-    return (nums[i] + nums[j] + nums[k]);
-if (target < 0)
-{
-    if ((nums[i] + nums[j] + nums[k]) < target)
-    {
-if (abs(target - (nums[i] + nums[j] + nums[k])) < abs(target - answer))answer = (nums[i] + nums[j] + nums[k]); 
-    } 
-    else if ((nums[i] + nums[j] + nums[k]) > target)
-    {
-if (abs(target - (nums[i] + nums[j] + nums[k])) < abs(target - answer)) answer = (nums[i] + nums[j] + nums[k]);
-    } 
-}
-    else if (target == 0)
-    {
-    if (abs((nums[i] + nums[j] + nums[k])) < abs(answer)) answer = (nums[i] + nums[j] + nums[k]);
-    }
-    else if (target > 0)
-    {
-if (abs(target - (nums[i] + nums[j] + nums[k])) < abs(target - answer)) answer = (nums[i] + nums[j] + nums[k]);
-    }
-        }
-        }
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                for (int k = j + 1; k < nums.size(); k++)
+                {
+                    if ((nums[i] + nums[j] + nums[k]) == target)
+                        return (nums[i] + nums[j] + nums[k]);
+                    if (target < 0)
+                    {
+                        if ((nums[i] + nums[j] + nums[k]) < target)
+                        {
+                            if (abs(target - (nums[i] + nums[j] + nums[k])) < abs(target - answer))answer = (nums[i] + nums[j] + nums[k]); 
+                        } 
+                        else if ((nums[i] + nums[j] + nums[k]) > target)
+                        {
+                            if (abs(target - (nums[i] + nums[j] + nums[k])) < abs(target - answer)) answer = (nums[i] + nums[j] + nums[k]);
+                        } 
+                    }
+                    else if (target == 0)
+                    {
+                        if (abs((nums[i] + nums[j] + nums[k])) < abs(answer)) answer = (nums[i] + nums[j] + nums[k]);
+                    }
+                    else if (target > 0)
+                    {
+                        if (abs(target - (nums[i] + nums[j] + nums[k])) < abs(target - answer)) answer = (nums[i] + nums[j] + nums[k]);
+                    }
+                }
+            }
         }
     return answer;
     }
