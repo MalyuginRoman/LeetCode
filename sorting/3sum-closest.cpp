5b0aac873e6c32b5b49ca1bcddc88d83965625b7
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-18 20:00:41 +0500

    LC-3sum-closest [Runtime: N/A; Memory: N/A]

--- sorting/3sum-closest.cpp
+++ sorting/3sum-closest.cpp
@@ -1,17 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
+    int threeSumClosest(vector<int>& nums, int target)
+    {
         vector<int> result;
-        for (int i = 0; i < nums.size(); i++)
+        for (int i = 0; i < nums.size()-2; i++)
         {
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                for (int k = j + 1; k < nums.size(); k++)
-                {
-                    int sum = nums[i] + nums[j] + nums[k];
+            //for (int j = i + 1; j < nums.size(); j++)
+            //{
+                //for (int k = j + 1; k < nums.size(); k++)
+                //{
+                    int sum = nums[i] + nums[/*j*/i+1] + nums[/*k*/i+2];
                     result.emplace_back(sum);
-                }
-            }
+                //}
+            //}
         }
         int answer = result[0];
         for (int i = 0; i < result.size(); i++)
