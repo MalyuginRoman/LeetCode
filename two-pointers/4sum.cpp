33d4a95a9e8096c40b52d7d8629bf15452343366
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-21 09:37:04 +0500

    LC-4sum [Runtime: 20 ms; Memory: 17.5 MB]

--- two-pointers/4sum.cpp
+++ two-pointers/4sum.cpp
@@ -6,38 +6,20 @@ public:
         bool isWrite = false;
         vector<vector<int>> ans;
         for(int i=0;i<n;i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue; // Skip duplicate i
             for(int j=i+1;j<n;j++) {
+                if(j > i + 1 && nums[j] == nums[j-1]) continue; // Skip duplicate j
                 int l=j+1;
                 int r=n-1;
-                long long sum=target-nums[i]-nums[j];
                 while(l<r) {
-                    long long total=nums[l]+nums[r];
-                    if(total==sum) {
-                        if (ans.size() == 0)
-                            ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        else
-                        {
-                            for (size_t n = 0; n < ans.size(); n++)
-                            {
-                                if (nums[i] == ans[n][0] &&
-                                    nums[j] == ans[n][1] &&
-                                    nums[l] == ans[n][2] &&
-                                    nums[r] == ans[n][3])
-                                    {
-                                        isWrite = true;
-                                        break;
-                                    }
-                                else
-                                        isWrite = false;
-                            }
-                            if (!isWrite)
-                                ans.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        }
+                    long long total= (long long)nums[i]+nums[j]+nums[l]+nums[r];
+                    if(total==target) {
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
                         l++;
                         r--;
                         while(l<r && nums[l]==nums[l-1]) l++;
                         while(l<r && nums[r]==nums[r+1]) r--;
-                    } else if(total>sum) {
+                    } else if(total>target) {
                         r--;
                     } else {
                         l++;
