2bb68464703b9553efdb3e447685bfaee32db8b4
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-23 21:30:11 +0500

    LC-24-game [Runtime: N/A; Memory: N/A]

--- array/24-game.cpp
+++ array/24-game.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool judgePoint24(std::vector<int>& cards) {
         bool result = false;
-        //sort(cards.begin(), cards.end());
+        sort(cards.begin(), cards.end());
         std::vector<double> a1;
         std::vector<double> a2;
+        std::vector<double> a3;
         for (int i = 3; i >= 0; i--)
         {
             for (int j = 3; j >= 0; j--)
@@ -22,12 +23,34 @@ public:
                             for (int t = 3; t >= 0; t--)
                                 if (t != i && t != j && t != k)
                                 {
+                                    for (int l = 0; l < a1.size(); l++)
+                                    {
+                                        a3.push_back(cards[t]);
+                                        a3.push_back(cards[k]);
+     if ((abs(a3[0] - a1[l]) > 1e-6) && abs((a3[1] / (a3[0] - a1[l])) - 24) < 1e-6) return true;
+else if ((abs(a3[0] - a1[l]) > 1e-6) && abs((a3[1] * (a3[0] - a1[l])) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a3[0] - a1[l]) / a3[1]) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a3[0] - a1[l]) * a3[1]) - 24) < 1e-6) return true;
+else if ((abs(a3[0] + a1[l]) > 1e-6) && abs((a3[1] / (a3[0] + a1[l])) - 24) < 1e-6) return true;
+else if ((abs(a3[0] + a1[l]) > 1e-6) && abs((a3[1] * (a3[0] + a1[l])) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a3[0] + a1[l]) / a3[1]) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a3[0] + a1[l]) * a3[1]) - 24) < 1e-6) return true;
+else if ((abs(a1[l] - a3[0]) > 1e-6) && abs((a3[1] / (a1[l] - a3[0])) - 24) < 1e-6) return true;
+else if ((abs(a1[l] - a3[0]) > 1e-6) && abs((a3[1] * (a1[l] - a3[0])) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a1[l] - a3[0]) / a3[1]) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a1[l] - a3[0]) * a3[1]) - 24) < 1e-6) return true;
+else if ((abs(a1[l] + a3[0]) > 1e-6) && abs((a3[1] / (a1[l] + a3[0])) - 24) < 1e-6) return true;
+else if ((abs(a1[l] + a3[0]) > 1e-6) && abs((a3[1] * (a1[l] + a3[0])) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a1[l] + a3[0]) / a3[1]) - 24) < 1e-6) return true;
+else if ((abs(a3[1])         > 1e-6) && abs(((a1[l] + a3[0]) * a3[1]) - 24) < 1e-6) return true;
+                                        a3.clear();
+                                    }
                                     a2.push_back(cards[k] + cards[t]);
                                     a2.push_back(cards[k] - cards[t]);
                                     a2.push_back(cards[t] - cards[k]);
                                     a2.push_back(cards[k] * cards[t]);
-                                    if (abs(cards[t]) > 0) a2.push_back(cards[k] / cards[t]);
-                                    if (abs(cards[k]) > 0) a2.push_back(cards[t] / cards[k]);
+                                    if (abs(cards[t]) > 1e-6) a2.push_back(cards[k] / cards[t]);
+                                    if (abs(cards[k]) > 1e-6) a2.push_back(cards[t] / cards[k]);
                                     for (int l = 0; l < a1.size(); l++)
                                     {
                                         for (int m = 0; m < a2.size(); m++)
