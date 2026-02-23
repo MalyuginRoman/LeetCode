163430ff29ce4d5f1fbd26e5fb3b960f15f3d66a
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-23 20:32:58 +0500

    LC-24-game [Runtime: N/A; Memory: N/A]

--- math/24-game.cpp
+++ math/24-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool judgePoint24(std::vector<int>& cards) {
         bool result = false;
-        sort(cards.begin(), cards.end());
+        //sort(cards.begin(), cards.end());
         std::vector<double> a1;
         std::vector<double> a2;
         for (int i = 3; i >= 0; i--)
@@ -13,8 +13,10 @@ public:
                 {
                     a1.push_back(cards[i] + cards[j]);
                     a1.push_back(cards[i] - cards[j]);
+                    a1.push_back(cards[j] - cards[i]);
                     a1.push_back(cards[i] * cards[j]);
-                    a1.push_back(cards[i] / cards[j]);
+                    if (abs(cards[i]) > 0) a1.push_back(cards[j] / cards[i]);
+                    if (abs(cards[j]) > 0) a1.push_back(cards[i] / cards[j]);
                     for (int k = 3; k >= 0; k--)
                         if (k != i && k != j)
                             for (int t = 3; t >= 0; t--)
@@ -22,16 +24,20 @@ public:
                                 {
                                     a2.push_back(cards[k] + cards[t]);
                                     a2.push_back(cards[k] - cards[t]);
+                                    a2.push_back(cards[t] - cards[k]);
                                     a2.push_back(cards[k] * cards[t]);
-                                    a2.push_back(cards[k] / cards[t]);
+                                    if (abs(cards[t]) > 0) a2.push_back(cards[k] / cards[t]);
+                                    if (abs(cards[k]) > 0) a2.push_back(cards[t] / cards[k]);
                                     for (int l = 0; l < a2.size(); l++)
                                     {
                                         for (int m = 0; m < a2.size(); m++)
                                         {
                                             if (abs((a1[l] + a2[m]) - 24) < 1e-6) return true;
                                             else if (abs((a1[l] - a2[m]) - 24) < 1e-6) return true;
+                                            else if (abs((a2[m] - a1[l]) - 24) < 1e-6) return true;
                                             else if (abs((a1[l] * a2[m]) - 24) < 1e-6) return true;
                                             else if (abs((a1[l] / a2[m]) - 24) < 1e-6) return true;
+                                            else if (abs((a2[m] / a1[l]) - 24) < 1e-6) return true;
                                         }
                                     }
                                     a2.clear();
