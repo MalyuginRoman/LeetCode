d73171c0be39ea45fa34c39cdbcfa848adb4a8cb
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-02-28 18:38:18 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

---
+++ math/rectangle-area.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int S1 = abs(ax2-ax1)*abs(ay2-ay1);
+        int S2 = abs(bx2-bx1)*abs(by2-by1);
+        int S3 = 0;
+        int result = 0;
+        if ((ax1==bx1)&&(ay1==by1)&&(ax2==bx2)&&(ay2==by2))
+        {
+            result = S1;
+            return result;
+        }
+        if ((bx1<ax2)&&(bx1>ax1)&&(by2<ay2)&&(by2>ay1) && S1!=0 && S2!=0)
+        {
+            S3 = abs(ax2-bx1)*abs(by2-ay1);
+        }
+        result = S1+S2-S3;
+        return result;
+    }
+};
