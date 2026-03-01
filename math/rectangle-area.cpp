02ce0344be7a32bafb2cf8ee87f9c55123aafe47
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 12:43:30 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

--- math/rectangle-area.cpp
+++ math/rectangle-area.cpp
@@ -5,7 +5,19 @@ public:
         int S1 = abs(ax2-ax1)*abs(ay2-ay1);
         int S2 = abs(bx2-bx1)*abs(by2-by1);
         int S3 = 0;
-        if ((ax2 > bx1 && ax2 < bx2) && (by2 > ay1 && by2 < ay2) &&         // vl
+        if (ax1 == bx1 && ay1 == by1 && ax2 == bx2 && ay2 == by2 &&    //equal
+            S1 != 0 && S2 != 0) {
+            S2 = 0;
+            S3 = 0; }
+        else if ((ax2 > bx1 && ax2 < bx2) && (ax1 > bx1 && ax1 < bx2) &&    //in1
+            (ay2 > by1 && ay2 < by2) && (ay1 > by1 && ay1 < by2) && S1 != 0 && S2 != 0) {
+            S1 = 0;
+            S3 = 0; }
+        else if ((bx2 > ax1 && bx2 < ax2) && (bx1 > ax1 && bx1 < ax2) &&    //in2
+            (by2 > ay1 && by2 < ay2) && (by1 > ay1 && by1 < ay2) && S1 != 0 && S2 != 0) {
+            S2 = 0;
+            S3 = 0; }
+        else if ((ax2 > bx1 && ax2 < bx2) && (by2 > ay1 && by2 < ay2) &&         // vl
             (bx1 > ax1) && (by1 < ay1) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(by2-ay1);
         else if ((ax2 > bx1 && ax2 < bx2) && (by1 > ay1 && by1 < ay2) &&    // nl
@@ -41,18 +53,6 @@ public:
         else if (ay1 > by1 && ay2 < by2 && (ax1 > bx1 && ax1 < bx2) &&      // l2
             (ax2 > bx2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(by2-by1);
-        else if ((ax2 > bx1 && ax2 < bx2) && (ax1 > bx1 && ax1 < bx2) &&    //in1
-            (ay2 > by1 && ay2 < by2) && (ay1 > by1 && ay1 < by2) && S1 != 0 && S2 != 0) {
-            S1 = 0;
-            S3 = 0; }
-        else if ((bx2 > ax1 && bx2 < ax2) && (bx1 > ax1 && bx1 < ax2) &&    //in2
-            (by2 > ay1 && by2 < ay2) && (by1 > ay1 && by1 < ay2) && S1 != 0 && S2 != 0) {
-            S2 = 0;
-            S3 = 0; }
-        else if (ax1 == bx1 && ay1 == by1 && ax2 == bx2 && ay2 == by2 &&    //equal
-            S1 != 0 && S2 != 0) {
-            S2 = 0;
-            S3 = 0; }
         result = S1 + S2 - S3;
         return result;
     }
