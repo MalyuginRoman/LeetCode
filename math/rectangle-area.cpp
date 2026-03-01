81bcf79cbbf628b572981cf8763e5443004dd163
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 12:42:07 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

--- math/rectangle-area.cpp
+++ math/rectangle-area.cpp
@@ -18,17 +18,29 @@ public:
             (bx2 < ax2) && (by1 < ay1) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-ax1)*abs(ay1-by2);
         else if (ax1 < bx1 && ax2 > bx2 && (ay1 > by1 && ay1 < by2) &&      // v1
-            S1 != 0 && S2 != 0)
+            (ay2 > by2) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-bx1)*abs(ay1-by2);
         else if (bx1 < ax1 && bx2 > ax2 && (ay1 > by1 && ay1 < by2) &&      // v2
-            S1 != 0 && S2 != 0)
+            (ay2 > by2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-ax1)*abs(ay1-by2);
         else if (ay1 < by1 && ay2 > by2 && (bx1 > ax1 && bx1 < ax2) &&      // r1
-            S1 != 0 && S2 != 0)
+            (ax1 < bx1) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(by2-by1);
         else if (ay1 > by1 && ay2 < by2 && (bx1 > ax1 && bx1 < ax2) &&      // r2
-            S1 != 0 && S2 != 0)
+            (ax1 < bx1) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(ay2-ay1);
+        else if (ax1 < bx1 && ax2 > bx2 && (ay2 > by1 && ay2 < by2) &&      // n1
+            (ay1 < by1) && S1 != 0 && S2 != 0)
+            S3 = abs(ax2-ax1)*abs(ay2-by1);
+        else if (bx1 > ax1 && bx2 < ax2 && (ay2 > by1 && ay2 < by2) &&      // n2
+            (ay1 > by1) && S1 != 0 && S2 != 0)
+            S3 = abs(bx2-bx1)*abs(ay2-by1);
+        else if (ay1 < by1 && ay2 > by2 && (ax1 > bx1 && ax1 < bx2) &&      // l1
+            (ax2 > bx2) && S1 != 0 && S2 != 0)
+            S3 = abs(bx2-ax1)*abs(ay2-ay1);
+        else if (ay1 > by1 && ay2 < by2 && (ax1 > bx1 && ax1 < bx2) &&      // l2
+            (ax2 > bx2) && S1 != 0 && S2 != 0)
+            S3 = abs(ax2-bx1)*abs(by2-by1);
         else if ((ax2 > bx1 && ax2 < bx2) && (ax1 > bx1 && ax1 < bx2) &&    //in1
             (ay2 > by1 && ay2 < by2) && (ay1 > by1 && ay1 < by2) && S1 != 0 && S2 != 0) {
             S1 = 0;
