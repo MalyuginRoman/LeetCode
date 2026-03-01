a4949c15e0529a0f83b492f77e994fb4b85d7ac1
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 17:34:08 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

--- geometry/rectangle-area.cpp
+++ geometry/rectangle-area.cpp
@@ -24,25 +24,25 @@ public:
         else if (ax1 <= bx1 && ax2 >= bx2 && (ay1 > by1 && ay1 < by2) &&      // v1
             (ay2 > by2) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-bx1)*abs(ay1-by2);
-        else if (bx1 < ax1 && bx2 > ax2 && (ay1 > by1 && ay1 < by2) &&      // v2
+        else if (bx1 <= ax1 && bx2 >= ax2 && (ay1 > by1 && ay1 < by2) &&      // v2
             (ay2 > by2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-ax1)*abs(ay1-by2);
         else if (ay1 <= by1 && ay2 >= by2 && (bx1 > ax1 && bx1 < ax2) &&      // r1
             (ax1 < bx1) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(by2-by1);
-        else if (ay1 > by1 && ay2 < by2 && (bx1 > ax1 && bx1 < ax2) &&      // r2
+        else if (ay1 >= by1 && ay2 <= by2 && (bx1 > ax1 && bx1 < ax2) &&      // r2
             (ax1 < bx1) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(ay2-ay1);
         else if (ax1 <= bx1 && ax2 >= bx2 && (ay2 > by1 && ay2 < by2) &&      // n1
             (ay1 < by1) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-bx1)*abs(ay2-by1);
-        else if (ax1 > bx1 && ax2 < bx2 && (ay2 > by1 && ay2 < by2) &&      // n2
+        else if (ax1 >= bx1 && ax2 <= bx2 && (ay2 > by1 && ay2 < by2) &&      // n2
             (ay1 < by1) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-ax1)*abs(ay2-by1);
         else if (ay1 <= by1 && ay2 >= by2 && (ax1 > bx1 && ax1 < bx2) &&      // l1
             (ax2 > bx2) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-ax1)*abs(by2-by1);
-        else if (ay1 > by1 && ay2 < by2 && (ax1 > bx1 && ax1 < bx2) &&      // l2
+        else if (ay1 >= by1 && ay2 <= by2 && (ax1 > bx1 && ax1 < bx2) &&      // l2
             (ax2 > bx2) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-ax1)*abs(ay2-ay1);
         else if ((ax2 > bx1 && ax2 < bx2) && (by2 > ay1 && by2 < ay2) &&    // vl
