ac264b8f80358f6d1ff20de2e510a0f17d378af5
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 17:54:49 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

--- geometry/rectangle-area.cpp
+++ geometry/rectangle-area.cpp
@@ -9,7 +9,7 @@ public:
             S1 != 0 && S2 != 0) {
             S2 = 0;
             S3 = 0; }
-        if (ax1 < bx1 && ay1 > by1 && ax2 > bx2 && ay2 < by2 &&    //X1
+        else if (ax1 < bx1 && ay1 > by1 && ax2 > bx2 && ay2 < by2 &&    //X1
             S1 != 0 && S2 != 0)
             S3 = abs(bx2-bx1)*abs(ay2-ay1);
         else if (ax1 > bx1 && ay1 < by1 && ax2 < bx2 && ay2 > by2 &&    //X2
@@ -30,7 +30,7 @@ public:
             (ay2 > by2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-ax1)*abs(ay1-by2);
         else if (ax1 < bx1 && ax2 > bx2 && (ay1 >= by1 && ay1 <= by2) &&      // v3
-            (ay2 < by2 && ay2 < by1) && S1 != 0 && S2 != 0)
+            (ay2 < by2 && ay2 > by1) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-bx1)*abs(ay2-by1);
         else if (ay1 <= by1 && ay2 >= by2 && (bx1 > ax1 && bx1 < ax2) &&      // r1
             (ax1 < bx1) && S1 != 0 && S2 != 0)
