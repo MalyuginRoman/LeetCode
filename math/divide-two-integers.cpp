4c7b716277d25de53d9eaf640fd2905dab97ada1
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 18:01:18 +0500

    LC-divide-two-integers [Runtime: N/A; Memory: N/A]

---
+++ math/divide-two-integers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long int result = 0;
+        long int val = 0;
+        if (dividend == 0) return 0;
+        else if (dividend == divisor) return 1;
+        else if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        else if (divisor == 1) return dividend;
+        else if (divisor == -1) return -dividend;
+        else if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0))
+            while (val < (dividend - divisor)) {
+                result += 1;
+                val += divisor;
+            }
+        else if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0))
+            while (val < (dividend + divisor)) {
+                result -= 1;
+                val -= divisor;
+            }
+        return result;
+    }
+};
