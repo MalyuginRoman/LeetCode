48a56cb5b728b2b826b529eeba9c6ba6f02d2b42
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 11:30:15 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

--- geometry/rectangle-area.cpp
+++ geometry/rectangle-area.cpp
@@ -8,23 +8,23 @@ public:
         if ((ax2 >= bx1 && ax2 <= bx2) && (by2 >= ay1 && by2 <= ay2) &&
             (bx2 > ax2) && (by1 < ay1) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(by2-ay1);
-        if ((ax2 >= bx1 && ax2 <= bx2) && (by1 >= ay1 && by1 <= ay2) &&
+        else if ((ax2 >= bx1 && ax2 <= bx2) && (by1 >= ay1 && by1 <= ay2) &&
             (bx2 > ax2) && (by2 > ay2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(ay2-by1);
-        if ((ax1 >= bx1 && ax1 <= bx2) && (by1 >= ay1 && by1 <= ay2) &&
+        else if ((ax1 >= bx1 && ax1 <= bx2) && (by1 >= ay1 && by1 <= ay2) &&
             (by2 > ay2) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-ax1)*abs(ay2-by1);
-        if ((ax1 >= bx1 && ax1 <= bx2) && (ay1 >= by1 && ay1 <= by2) &&
+        else if ((ax1 >= bx1 && ax1 <= bx2) && (ay1 >= by1 && ay1 <= by2) &&
             (by1 < ay1) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-ax1)*abs(ay1-by2);
-        if (ax1 <= bx1 && ax2 <= bx2 && (ay1 >= by1 && ay1 <= by2) &&
+        else if (ax1 <= bx1 && ax2 <= bx2 && (ay1 >= by1 && ay1 <= by2) &&
             (ay2 >= by2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-ax1)*abs(ay1-by2);
-        if ((ax2 >= bx1 && ax2 <= bx2) && (ax1 >= bx1 && ax1 <= bx2) &&
+        else if ((ax2 >= bx1 && ax2 <= bx2) && (ax1 >= bx1 && ax1 <= bx2) &&
             (ay2 >= by1 && ay2 <= by2) && (ay1 >= by1 && ay1 <= by2) && S1 != 0 && S2 != 0) {
             S1 = 0;
             S3 = 0; }
-        if ((bx2 >= ax1 && bx2 <= ax2) && (bx1 >= ax1 && bx1 <= ax2) &&
+        else if ((bx2 >= ax1 && bx2 <= ax2) && (bx1 >= ax1 && bx1 <= ax2) &&
             (by2 >= ay1 && by2 <= ay2) && (by1 >= ay1 && by1 <= ay2) && S1 != 0 && S2 != 0) {
             S2 = 0;
             S3 = 0; }
