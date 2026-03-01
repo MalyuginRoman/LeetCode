2c101d0e05b461d909519a3a8b314750a7e599ca
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 12:59:44 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

--- math/rectangle-area.cpp
+++ math/rectangle-area.cpp
@@ -5,6 +5,9 @@ public:
         int S1 = abs(ax2-ax1)*abs(ay2-ay1);
         int S2 = abs(bx2-bx1)*abs(by2-by1);
         int S3 = 0;
+        bool b1 = ax1 > bx1;
+        bool b2 = ax2 < bx2;
+        bool b3 = (ay2 > by1 && ay2 < by2);
         if (ax1 == bx1 && ay1 == by1 && ax2 == bx2 && ay2 == by2 &&    //equal
             S1 != 0 && S2 != 0) {
             S2 = 0;
@@ -17,18 +20,6 @@ public:
             (by2 > ay1 && by2 < ay2) && (by1 > ay1 && by1 < ay2) && S1 != 0 && S2 != 0) {
             S2 = 0;
             S3 = 0; }
-        else if ((ax2 > bx1 && ax2 < bx2) && (by2 > ay1 && by2 < ay2) &&         // vl
-            (bx1 > ax1) && (by1 < ay1) && S1 != 0 && S2 != 0)
-            S3 = abs(ax2-bx1)*abs(by2-ay1);
-        else if ((ax2 > bx1 && ax2 < bx2) && (by1 > ay1 && by1 < ay2) &&    // nl
-            (bx1 > ax1) && (by2 > ay2) && S1 != 0 && S2 != 0)
-            S3 = abs(ax2-bx1)*abs(ay2-by1);
-        else if ((ax1 > bx1 && ax1 < bx2) && (by1 > ay1 && by1 < ay2) &&    // nr
-            (bx1 < ax1) && (by2 > ay2) && S1 != 0 && S2 != 0)
-            S3 = abs(bx2-ax1)*abs(ay2-by1);
-        else if ((ax1 > bx1 && ax1 < bx2) && (ay1 > by1 && ay1 < by2) &&    // vr
-            (bx2 < ax2) && (by1 < ay1) && S1 != 0 && S2 != 0)
-            S3 = abs(bx2-ax1)*abs(ay1-by2);
         else if (ax1 < bx1 && ax2 > bx2 && (ay1 > by1 && ay1 < by2) &&      // v1
             (ay2 > by2) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-bx1)*abs(ay1-by2);
@@ -43,16 +34,28 @@ public:
             S3 = abs(ax2-bx1)*abs(ay2-ay1);
         else if (ax1 < bx1 && ax2 > bx2 && (ay2 > by1 && ay2 < by2) &&      // n1
             (ay1 < by1) && S1 != 0 && S2 != 0)
-            S3 = abs(ax2-ax1)*abs(ay2-by1);
-        else if (bx1 > ax1 && bx2 < ax2 && (ay2 > by1 && ay2 < by2) &&      // n2
-            (ay1 > by1) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-bx1)*abs(ay2-by1);
+        else if (ax1 > bx1 && ax2 < bx2 && (ay2 > by1 && ay2 < by2) &&      // n2
+            (ay1 < by1) && S1 != 0 && S2 != 0)
+            S3 = abs(ax2-ax1)*abs(ay2-by1);
         else if (ay1 < by1 && ay2 > by2 && (ax1 > bx1 && ax1 < bx2) &&      // l1
             (ax2 > bx2) && S1 != 0 && S2 != 0)
             S3 = abs(bx2-ax1)*abs(ay2-ay1);
         else if (ay1 > by1 && ay2 < by2 && (ax1 > bx1 && ax1 < bx2) &&      // l2
             (ax2 > bx2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(by2-by1);
+        else if ((ax2 > bx1 && ax2 < bx2) && (by2 > ay1 && by2 < ay2) &&    // vl
+            (bx1 > ax1) && (by1 < ay1) && S1 != 0 && S2 != 0)
+            S3 = abs(ax2-bx1)*abs(by2-ay1);
+        else if ((ax2 > bx1 && ax2 < bx2) && (by1 > ay1 && by1 < ay2) &&    // nl
+            (bx1 > ax1) && (by2 > ay2) && S1 != 0 && S2 != 0)
+            S3 = abs(ax2-bx1)*abs(ay2-by1);
+        else if ((ax1 > bx1 && ax1 < bx2) && (by1 > ay1 && by1 < ay2) &&    // nr
+            (bx1 < ax1) && (by2 > ay2) && S1 != 0 && S2 != 0)
+            S3 = abs(bx2-ax1)*abs(ay2-by1);
+        else if ((ax1 > bx1 && ax1 < bx2) && (ay1 > by1 && ay1 < by2) &&    // vr
+            (bx2 < ax2) && (by1 < ay1) && S1 != 0 && S2 != 0)
+            S3 = abs(bx2-ax1)*abs(ay1-by2);
         result = S1 + S2 - S3;
         return result;
     }
