ad7cd04d44bad7cca0e682925caf5788ed5e25dc
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-01 11:07:33 +0500

    LC-rectangle-area [Runtime: N/A; Memory: N/A]

--- geometry/rectangle-area.cpp
+++ geometry/rectangle-area.cpp
@@ -9,6 +9,8 @@ public:
             S3 = abs(ax2-bx1)*abs(by2-ay1);
         if ((ax2 >= bx1 && ax2 <= bx2) && (by1 >= ay1 && by1 <= ay2) && S1 != 0 && S2 != 0)
             S3 = abs(ax2-bx1)*abs(ay2-by1);
+        if ((ax1 >= bx1 && ax1 <= bx2) && (by1 >= ay1 && by1 <= ay2) && S1 != 0 && S2 != 0)
+            S3 = abs(bx2-ax1)*abs(ay2-by1);
         if ((ax2 >= bx1 && ax2 <= bx2) && (ax1 >= bx1 && ax1 <= bx2) && (ay2 >= by1 && ay2 <= by2) && (ay1 >= by1 && ay1 <= by2) && S1 != 0 && S2 != 0) {
             S1 = 0;
             S3 = 0; }
