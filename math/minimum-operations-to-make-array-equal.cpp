88f39ad9eec8cb75f7ce8296eaf431df7f70b060
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-03 22:40:51 +0500

    LC-minimum-operations-to-make-array-equal [Runtime: 0 ms; Memory: 8.1 MB]

--- math/minimum-operations-to-make-array-equal.cpp
+++ math/minimum-operations-to-make-array-equal.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int minOperations(int n) {
-        int result = 0;
-        if (n == 3) result = 2;
-        else
-        {
-            int count = n/2;
-            if (n % 2 == 1) count += 1;
-            for (int i = 0; i < count; i++)
-                result += 2*i+1;
-        }
-        return result;   
+        return n * n / 4;   
     }
 };
