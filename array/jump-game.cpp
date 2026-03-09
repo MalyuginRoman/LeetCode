8ccf950343fef176c9bcbc202146841374652667
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-09 18:58:27 +0500

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- array/jump-game.cpp
+++ array/jump-game.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i)
+        for (int i = 0; i < nums.size(); i++)
         {
-            if (nums[i] == 0) return false;
+            if (nums[i] == 0 && nums.size() != 0) return false;
         }
         return true;
     }
