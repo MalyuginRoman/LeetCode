03456c0b3575a90f0eff2d353dab4c34414a68a3
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-09 19:01:36 +0500

    LC-jump-game [Runtime: 0 ms; Memory: 52.3 MB]

--- greedy/jump-game.cpp
+++ greedy/jump-game.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == 0 && nums.size() != 1) return false;
+        int maxReach = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
         }
         return true;
     }
