1c312f7f3f2a0a0ee2f43a747fbaaa4e76240be1
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-18 19:10:59 +0500

    LC-sqrtx [Runtime: N/A; Memory: N/A]

---
+++ binary-search/sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int result = 1;
+        unsigned long int val = 10E+8;
+        bool isOk = false;
+        bool reev = false;
+        while (!isOk)
+        {
+            unsigned long int current = val * val;
+            if (current >= x && !reev)
+            {
+                result = val;
+                val /= 10;
+            }
+            else if (current <= x)
+            {
+                result = val;
+                val += 1;
+                reev = true;
+            }
+            else if (current > x && reev)
+                isOk = true;
+        }
+        return result;
+    }
+};
