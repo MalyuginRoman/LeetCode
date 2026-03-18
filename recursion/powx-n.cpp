dd84a4722a667721540b77a51ff9ec691d3d8a5f
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-18 20:49:49 +0500

    LC-powx-n [Runtime: 175 ms; Memory: 8.6 MB]

--- recursion/powx-n.cpp
+++ recursion/powx-n.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double result = 1;
+        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
+        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
         if (x == 1) return result;
         if (n > 0)
         {
-            for (long int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
                 result *= x;
         }
         else if (n < 0)
         {
-            for (long int i = 0; i < abs(n); i++)
+            for (int i = 0; i < abs(n); i++)
                 result *= x;
             result = 1/result;
         }
