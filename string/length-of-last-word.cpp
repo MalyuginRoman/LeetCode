c773ad41bc56f86ff8224d3dde1e5cee3da70ad0
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-20 23:01:39 +0500

    LC-length-of-last-word [Runtime: 0 ms; Memory: 8.9 MB]

---
+++ string/length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        bool isOk = false;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] != ' ') {
+                isOk = true;
+                length++;
+            }
+            else if (isOk) {
+                break;
+            }
+        }
+        return length;
+    }
+};
