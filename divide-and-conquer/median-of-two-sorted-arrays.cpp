882d972fd7a8d1de865349711c91141e3a554281
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-21 23:21:02 +0500

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/median-of-two-sorted-arrays.cpp
+++ divide-and-conquer/median-of-two-sorted-arrays.cpp
@@ -4,26 +4,35 @@ public:
         int count1 = nums1.size();
         double sum1 = 0;
         double ave1 = 0;
-        if (count1 > 1)
+        double a = 0;
+        if (count1 > 0)
         {
-            for (int i = 0; i < count1; i++)
-                sum1 += nums1[i];
-            ave1 = sum1/count1;
+            a++;
+            if (count1 > 1)
+            {
+                for (int i = 0; i < count1; i++)
+                    sum1 += nums1[i];
+                ave1 = sum1/count1;
+            }
+            else
+                ave1 = nums1[0];
         }
-        else
-            ave1 = nums1[0];
         int count2 = nums2.size();
         double sum2 = 0;
         double ave2 = 0;
-        if (count2 > 1)
+        if (count1 > 0)
         {
-            for (int i = 0; i < count2; i++)
-                sum2 += nums2[i];
-            ave2 = sum2/count2;
+            a++;
+            if (count2 > 1)
+            {
+                for (int i = 0; i < count2; i++)
+                    sum2 += nums2[i];
+                ave2 = sum2/count2;
+            }
+            else
+                ave2 = nums2[0];
         }
-        else
-            ave2 = nums2[0];
-        double result = (ave1 + ave2) / 2;
+        double result = (ave1 + ave2) / a;
         return result;
     }
 };
