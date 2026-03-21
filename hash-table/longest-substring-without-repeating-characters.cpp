95aca1cbd6027682f27644964cd0ec8a88fa73a3
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-21 18:22:31 +0500

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        std::string a1 = "";
+        std::string a2 = "";
+        int result = 0;
+        int count = s.size();
+        if (count == 0) return result;
+        for (int i = 0; i < count; i++)
+        {
+            char a = s[i];
+            if (a1.size() == 0)
+                a1 += a;
+            else
+            {
+                int count1 = a1.size();
+                for (int j = 0; j < count1; j++)
+                {
+                    char b = a1[j];
+                    if (a != b)
+                    {
+                        if (j == count1 - 1)
+                            a1 += a;
+                    }
+                    else
+                    {
+                        if (a1.size() > a2.size())
+                        {
+                            a2 = a1;
+                            a1 = "";
+                            a1 += a;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        result = std::max(a1.size(),a2.size());
+        return result;
+    }
+};
