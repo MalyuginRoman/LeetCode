5b080334de479d823be1f611d473a56b57aa511b
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-22 13:04:44 +0500

    LC-median-of-two-sorted-arrays [Runtime: 0 ms; Memory: 96 MB]

--- array/median-of-two-sorted-arrays.cpp
+++ array/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
-        int count1 = nums1.size();
-        double sum1 = 0;
-        double ave1 = 0;
-        double a = 0;
-        if (count1 > 0)
-        {
-            a++;
-            if (count1 > 1)
-            {
-                for (int i = 0; i < count1; i++)
-                    sum1 += nums1[i];
-                ave1 = sum1/count1;
-            }
-            else
-                ave1 = nums1[0];
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int> merged;
+        for (int i = 0; i < n; i++) {
+            merged.push_back(nums1[i]);
         }
-        int count2 = nums2.size();
-        double sum2 = 0;
-        double ave2 = 0;
-        if (count2 > 0)
-        {
-            a++;
-            if (count2 > 1)
-            {
-                for (int i = 0; i < count2; i++)
-                    sum2 += nums2[i];
-                ave2 = sum2/count2;
-            }
-            else
-                ave2 = nums2[0];
+        for (int i = 0; i < m; i++) {
+            merged.push_back(nums2[i]);
         }
-        double result = (ave1 + ave2) / a;
-        return result;
+        sort(merged.begin(), merged.end());
+        int total = merged.size();
+        if (total % 2 == 1) {
+            return (merged[total / 2]);
+        } else {
+            int middle1 = merged[total / 2 - 1];
+            int middle2 = merged[total / 2];
+            return (middle1 + middle2) / 2.0;
+        }
     }
 };
