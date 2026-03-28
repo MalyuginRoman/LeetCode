15cac533815ba587f8b17e93462fe1a9af831010
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-28 12:33:56 +0500

    LC-power-of-four [Runtime: 0 ms; Memory: 7.8 MB]

--- bit-manipulation/power-of-four.cpp
+++ bit-manipulation/power-of-four.cpp
@@ -4,9 +4,7 @@ public:
         bool result = false;
         double val = 1;
         while (val < n)
-        {
             val *= 4;
-        }
         if (val - n == 0)
             result = true;
         else if (val - n != 0)
