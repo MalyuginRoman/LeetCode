29527314d19d9d5fc45ffee5e99e35b0eb869bb2
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-03-28 12:28:31 +0500

    LC-power-of-four [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/power-of-four.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        bool result = false;
+        if (n == 1) return true;
+        while (n / 4 > 0)
+        {
+            n /= 4;
+            if (n / 4 <= 1 && n % 4 == 0)
+            {
+                result = true;
+                break;
+            }
+            else if (n / 4 <= 1 && n % 4 != 0)
+            {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+};
