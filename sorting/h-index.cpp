b5ab359b7f7023c12a81f6f8127a2bfabad12705
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-09 19:47:05 +0500

    LC-h-index [Runtime: N/A; Memory: N/A]

--- sorting/h-index.cpp
+++ sorting/h-index.cpp
@@ -3,6 +3,8 @@ public:
     int hIndex(vector<int>& citations) {
         std::vector<int> h;
         int result = 0;
+        if (citations.size() == 1 && citations[0] >= 1)
+            return 1;
         for (int i = 0; i < citations.size(); i++)
             h.push_back(0);
         for (int i = 0; i < citations.size(); i++)
