036b6c73c84c0225795f803508af11fd97edad1b
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-09 20:14:49 +0500

    LC-h-index-ii [Runtime: 0 ms; Memory: 22.6 MB]

--- array/h-index-ii.cpp
+++ array/h-index-ii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations.begin(),citations.end());
         int n = citations.size();
         int max = 0;
         for(int i=0; i<citations.size(); i++)
