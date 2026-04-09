1664020679e3f629c33de3d4835b0f5aec2e7e8b
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-09 19:44:33 +0500

    LC-h-index [Runtime: N/A; Memory: N/A]

---
+++ counting-sort/h-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        std::vector<int> h;
+        int result = 0;
+        for (int i = 0; i < citations.size(); i++)
+            h.push_back(0);
+        for (int i = 0; i < citations.size(); i++)
+            for (int j = 0; j < citations.size(); j++)
+            {
+                if (citations[i] >= j)
+                    h[j]++;
+                else
+                    break;
+            }
+        for (int i = 0; i < citations.size(); i++)
+            if (h[i] >= i) result = i;
+        return result;
+    }
+};
