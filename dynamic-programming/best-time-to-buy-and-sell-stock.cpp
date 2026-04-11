942b3ac3df8afceae4bc1d883a6af149bcd96ec2
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-11 09:18:31 +0500

    LC-best-time-to-buy-and-sell-stock [Runtime: N/A; Memory: N/A]

--- dynamic-programming/best-time-to-buy-and-sell-stock.cpp
+++ dynamic-programming/best-time-to-buy-and-sell-stock.cpp
@@ -3,31 +3,34 @@ public:
     int maxProfit(vector<int>& prices) {
         int result = 0;
         int max, min;
+        int mincand;
         int imax, imin;
         bool isOk = false;
         int count = prices.size();
         for (int i = 1; i < count; i++)
         {
-            int a1 = prices[i];
-            int a2 = prices[i - 1];
             if (prices[i] - prices[i - 1] > 0)
             {
                 if (!isOk)
                 {
                     isOk = true;
                     min = prices[i - 1];
+                    mincand = min;
                     imin = i - 1;
                     max = prices[i];
                     imax = i;
                 }
                 else
                 {
+                    if (prices[i - 1] < min)
+                        mincand = prices[i - 1];
                     if (max < prices[i] && imax < i)
                     {
                         max = prices[i];
                         imax = i;
+                        min = mincand;
                     }
-                    if (min > prices[i - 1] && (prices[i] - prices[i - 1]) >= (max - min))
+                    if (min > prices[i - 1] && (prices[i] - mincand) >= (max - min))
                     {
                         min = prices[i - 1];
                         imin = i - 1;
