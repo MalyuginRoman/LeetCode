9bf89dc6ab790fca53642a11018721c69e735004
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-12 09:51:28 +0500

    LC-set-matrix-zeroes [Runtime: N/A; Memory: N/A]

---
+++ matrix/set-matrix-zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> i0;
+        vector<int> j0;
+        for(int i = 0; i < matrix.size(); i++)
+            for(int j = 0; j < matrix[i].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    i0.push_back(i);
+                    j0.push_back(j);
+                }
+            }
+        for(int i = 0; i < matrix.size(); i++)
+            for(int j = 0; j < matrix[i].size(); j++)
+                for(int t = 0; t < i0.size(); t++)
+                    for(int k = 0; k < j0.size(); k++)
+                        if (i == i0[t] || j == j0[k])
+                            matrix[i][j] = 0;
+    }
+};
