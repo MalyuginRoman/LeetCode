03a0b55ff4767d718077971a2cf32e77af613a63
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-15 20:28:33 +0500

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/merge-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int i = m; i < (m + n); i++)
+            nums1[i] = nums2[i - m];
+        for (int i = 1; i < (m + n); i++)
+        {
+            if (nums1[i] < nums1[i - 1])
+            {
+                int val = nums1[i];
+                nums1[i] = nums1[i - 1];
+                nums1[i - 1] = val;
+            }
+        }
+    }
+};
