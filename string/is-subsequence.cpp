2648610e09aa703c81e1b224626dce792c05ae37
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-17 22:34:04 +0500

    LC-is-subsequence [Runtime: N/A; Memory: N/A]

--- string/is-subsequence.cpp
+++ string/is-subsequence.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int cc = s.size();
-        vector<bool> result (cc, false);
-        for (int i = 0; i < t.size(); i++)
-            for (int j = 0; j < s.size(); j++)
-                if (t[i] == s[j])
-                {
-                    result[j] = true;
-                    break;
-                }
-        if (count(result.begin(), result.end(), false) > 0)
-            return false;
-        else
-            return true;
+        int i = 0, j = 0;
+        while (i < t.size() && j < s.size())
+        {
+            if (t[i] == s[j])
+                i++;
+            j++;
+        }
+        return i == s.size();
     }
 };
