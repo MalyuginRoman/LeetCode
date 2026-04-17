585d8b61c99654c390bf59bac2e5bdc7bed70e61
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-17 22:08:27 +0500

    LC-3sum [Runtime: 1244 ms; Memory: 295.7 MB]

--- two-pointers/3sum.cpp
+++ two-pointers/3sum.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;
-        for (int i = 0; i < nums.size(); i++)
-        for (int j = 0; j < nums.size(); j++)
-        for (int k = 0; k < nums.size(); k++)
-        {
-            if (i != j && i != k && j != k && (nums[i]+nums[j]+nums[k]) == 0)
+        int target = 0;
+        sort(nums.begin(), nums.end());
+        set<vector<int>> s;
+        vector<vector<int>> output;
+        for (int i = 0; i < nums.size(); i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k)
             {
-                vector<int> value;
-                value.push_back(nums[i]);
-                value.push_back(nums[j]);
-                value.push_back(nums[k]);
-                sort(value.begin(), value.end());
-                if (count(result.begin(), result.end(), value) == 0)
-                    result.push_back(value);
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target)
+                {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } 
+                else if (sum < target)
+                    j++;
+                else
+                    k--;
             }
         }
-        return result;
+        for(auto triplets : s)
+            output.push_back(triplets);
+        return output;
     }
 };
