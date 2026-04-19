649d08166cfa46e1ae84d0626ee6b92e4226c659
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-19 13:23:25 +0500

    LC-wildcard-matching [Runtime: N/A; Memory: N/A]

--- greedy/wildcard-matching.cpp
+++ greedy/wildcard-matching.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         bool result = false;
-        if (p == "*") return true;
-        if (s.size() != p.size()) return false;
+        //if (p == "*") return true;
+        //if (s.size() != p.size() && count(p.begin(),p.end(),'*') < 0) return false;
         int count = p.size();
         int val = -1;
         for (int i = 0; i < count; i++)
         {
             if (p[i] != s[i] || s[i] == '?')
                 val++;
+            if (p[i] == '*') return true;
         }
         if (val == p.size()) result = true;
         return result;
