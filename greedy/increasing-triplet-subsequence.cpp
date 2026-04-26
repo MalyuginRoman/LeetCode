495fb56ce156e40e3a60be5236839e7d2b812336
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-04-26 21:49:04 +0500

    LC-increasing-triplet-subsequence [Runtime: N/A; Memory: N/A]

--- greedy/increasing-triplet-subsequence.cpp
+++ greedy/increasing-triplet-subsequence.cpp
@@ -4,11 +4,11 @@ public:
         bool result = false;
         int start = 0;
         int end = nums.size() - 1;
-        for (int i = start; i < (end - 2); i++)
+        for (int i = start; i <= (end - 2); i++)
         {
-            for (int k = end; k > (i + 2); k--)
+            for (int k = end; k >= (i + 2); k--)
             {
-                for (int j = (i + 1); j < (k - 1); j++)
+                for (int j = (i + 1); j <= (k - 1); j++)
                 {
                     if (nums[i] < nums[j] && nums[j] < nums[k])
                     {
