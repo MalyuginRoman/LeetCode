b595c29fc1629d3394790e8e5fdaa21745733c11
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-19 20:10:22 +0500

    LC-find-pivot-index [Runtime: N/A; Memory: N/A]

---
+++ prefix-sum/find-pivot-index.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int count = nums.size();
+        vector<int> l(count), r(count);
+        int result = -1;
+        int value = 0;
+        for (int i = 0; i < count; i++)
+        {
+            value += nums[i];
+            l[i] = value;
+        }
+        value = 0;
+        for (int i = count - 1; i >= 0 ; i--)
+        {
+            value += nums[i];
+            r[i] = value;
+        }
+        for (int i = 0; i < count - 1; i++)
+        {
+            if (l[i] == r[i])
+            {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+};
