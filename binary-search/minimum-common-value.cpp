7cc70d24228a1a53f36c2a2c9bebe3d25903000b
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-19 20:25:26 +0500

    LC-minimum-common-value [Runtime: N/A; Memory: N/A]

---
+++ binary-search/minimum-common-value.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int result = -1;
+        bool isOk = false;
+        int j = 0;
+        if (nums1[nums1.size() - 1] < nums2[0] || nums2[nums2.size() - 1] < nums1[0])
+            return result;
+        while (!isOk)
+        {
+            for (int i = 0; i < nums1.size(); i++)
+            {
+                if (nums1[i] == nums2[j])
+                {
+                    result = nums1[i];
+                    isOk = true;
+                    break;
+                }
+            }
+            j++;
+        }
+        return result;
+    }
+};
