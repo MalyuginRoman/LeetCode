3526e8e6106ac73c82c7639f5712a470bb988551
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-20 21:25:59 +0500

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

--- array/daily-temperatures.cpp
+++ array/daily-temperatures.cpp
@@ -7,8 +7,8 @@ public:
         {
             if (i == count - 1)
                 result[i] = 0;
-            else if (temperatures[i] == temperatures[i - 1])
-                result[i] = result[i - 1];
+            else if (temperatures[i] == temperatures[i + 1])
+                result[i] = result[i + 1] + 1;
             else
             {
                 int val = 0;
