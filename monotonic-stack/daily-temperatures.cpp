ab7915fa3d8c5db4c47c58fef0d670087839f978
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-20 21:20:15 +0500

    LC-daily-temperatures [Runtime: N/A; Memory: N/A]

---
+++ monotonic-stack/daily-temperatures.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int count = temperatures.size();
+        vector<int> result(count, - 9999);
+        for (int i = count - 1; i >= 0; i--)
+        {
+            if (i == count - 1)
+                result[i] = 0;
+            else
+            {
+                int val = 0;
+                for (int j = i + 1; j < count; j++)
+                {
+                    int a1 = temperatures[j];
+                    int a2 = temperatures[i];
+                    if (temperatures[j] > temperatures[i])
+                    {
+                        val++;
+                        break;
+                    }
+                    if (j != count - 1)
+                        val++;
+                }
+                result[i] = val;
+            }
+        }
+        return result;
+    }
+};
