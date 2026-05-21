d3848e796508c883891c46f29361cd5805559814
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-21 19:18:44 +0500

    LC-find-the-length-of-the-longest-common-prefix [Runtime: N/A; Memory: N/A]

---
+++ trie/find-the-length-of-the-longest-common-prefix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+    int result = 0;
+    vector<string> strArr1;
+    vector<string> strArr2;
+    for (int num : arr1)
+        if (count(strArr1.begin(), strArr1.end(), to_string(num)) < 1)
+            strArr1.push_back(to_string(num));
+    for (int num : arr2)
+        if (count(strArr1.begin(), strArr1.end(), to_string(num)) < 1)
+            strArr2.push_back(to_string(num));
+    
+        for (const string& xStr : strArr1)
+        {
+            for (const string& yStr : strArr2)
+            {
+                int value = 0;
+                int count = min(xStr.length(), yStr.length());
+                for (int i = 0; i < count; ++i)
+                {
+                    if (xStr[i] == yStr[i])
+                        value++;
+                    else
+                        break; // Stop at the first mismatch
+                }
+                result = max(result, value);
+            }
+        }
+    return result; 
+    }
+};
