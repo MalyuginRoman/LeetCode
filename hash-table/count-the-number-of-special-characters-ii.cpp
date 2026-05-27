cec8848ba77dd321ecc5280a39df2c707e447e43
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-27 22:19:25 +0500

    LC-count-the-number-of-special-characters-ii [Runtime: N/A; Memory: N/A]

---
+++ hash-table/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        int result = 0;
+        vector<char> chA = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        vector<char> chB = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        vector<int> numA(chA.size(), 0);
+        vector<int> numB(chB.size(), 0);
+        vector<bool> bolA(chA.size(), false);
+        vector<bool> bolB(chB.size(), false);
+        for (int i = 0; i < word.size(); i++)
+        {
+            for (int j = 0; j < chA.size(); j++)
+            {
+                char val = word[i];
+                char valA = chA[j];
+                char valB = chB[j];
+                if (val == valA)
+                {
+                    numA[j] = i;
+                    bolA[j] = true;
+                    break;
+                }
+                else if (val == valB)
+                {
+                    numB[j] = i;
+                    bolB[j] = true;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < numA.size(); i++)
+        {
+            if (numB[i] > numA[i] && bolA[i] == true)
+                result++;
+        }
+        return result;
+    }
+};
