687158fd7a4c4de4f476e8bebb5ed103b711fb41
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-30 22:23:45 +0500

    LC-concatenation-of-array [Runtime: N/A; Memory: N/A]

--- array/concatenation-of-array.cpp
+++ array/concatenation-of-array.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> ans;
-        for (int j = 0; j < 2; j ++)
-            for (int i = 0; i < nums.size(); i ++)
-                ans.push_back(nums[i]);
+        vector<int> ans = nums;
+        for (int j = 0; j < nums.size(); j++)
+            ans.push_back(nums[j]);
         return ans;
     }
 };
