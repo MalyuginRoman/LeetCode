8cb3584e61c7cf8f873dd7343a9d1e2d4897ae43
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-05-31 15:46:06 +0500

    LC-set-mismatch [Runtime: N/A; Memory: N/A]

--- sorting/set-mismatch.cpp
+++ sorting/set-mismatch.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> result;
-        int num, co = 0;
-        for (int i = 0; i < nums.size(); i ++)
-            if (count(nums.begin(), nums.end(), nums[i]) == 2)
+        vector<int> result (2, 0);
+        bool isOk1 = false;
+        bool isOk2 = false;
+        int count = nums.size() + 1;
+        vector<int> numN (count, 0);
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < (count - 1); i ++)
+            numN[nums[i]] += 1;
+        for (int i = 1; i < count; i ++)
+        {
+            if (numN[i] == 2)
             {
-                num = nums[i];
+                result[0] = i;
+                isOk1 = true;
+            }
+            if (numN[i] == 0)
+            {
+                result[1] = i;
+                isOk2 = true;
+            }
+            if (isOk1 && isOk2)
                 break;
-            }
-        for (int i = 0; i < nums.size(); i ++)
-            if (nums[i] == num)
-            {
-                result.push_back(i + 1);
-                co++;
-                if (co == 2) break;
-            }
+        }
         return result;
     }
 };
