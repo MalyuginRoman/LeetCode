ad359a215079e0145c96d7d19d4f5c0b2454a966
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-06-01 19:38:39 +0500

    LC-find-all-numbers-disappeared-in-an-array [Runtime: N/A; Memory: N/A]

--- array/find-all-numbers-disappeared-in-an-array.cpp
+++ array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> result;
+        /*vector<int> result;
         for (int i = 1; i < nums.size() + 1; i++)
             if (count(nums.begin(), nums.end(), i) == 0)
                 result.push_back(i);
-        return result;
+        return result;*/
+        unordered_set<int> arr(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 1; i <= n; i++) {
+            if(arr.find(i) == arr.end()) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
     }
 };
