7e20d11c473895cc08e997dfbfad2a27e52836e5
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-06-04 19:11:29 +0500

    LC-total-waviness-of-numbers-in-range-i [Runtime: N/A; Memory: N/A]

--- math/total-waviness-of-numbers-in-range-i.cpp
+++ math/total-waviness-of-numbers-in-range-i.cpp
@@ -9,7 +9,7 @@ public:
     }
     int totalWaviness(int num1, int num2) {
         int result = 0;
-        int n1, n2, n3, n4;
+        int n1, n2, n3, n4, n5;
         for (int i = num1; i <= num2; i++)
         {
             if (i / 10000 > 0)
@@ -18,10 +18,12 @@ public:
                 n2 = (i % 10000) / 1000;
                 n3 = ((i % 10000) % 1000) / 100;
                 n4 = (((i % 10000) % 1000) % 100) / 10;
-                n4 = (((i % 10000) % 1000) % 100) % 10;
+                n5 = (((i % 10000) % 1000) % 100) % 10;
                 if (isOk(n1, n2, n3))
                     result++;
                 if (isOk(n2, n3, n4))
+                    result++;
+                if (isOk(n3, n4, n5))
                     result++;
             }
             else if (i / 1000 > 0)
