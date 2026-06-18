f593e95ad40666129debea76fdaae47ab3247e1f
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-06-18 18:45:45 +0500

    LC-angle-between-hands-of-a-clock [Runtime: N/A; Memory: N/A]

--- math/angle-between-hands-of-a-clock.cpp
+++ math/angle-between-hands-of-a-clock.cpp
@@ -4,7 +4,7 @@ public:
         double min = static_cast<double>(minutes);
         double h = (hour % 12 + min / 60) * 30;
         double m = minutes * 6;
-        double result = abs(h - m) < abs(m - h) ? abs(h - m) : abs(m - h);
+        double result = abs(h - m) < 180 ? abs(h - m) : 360. - abs(h - m);
         return result;
     }
 };
