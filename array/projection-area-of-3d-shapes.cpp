5bb0b72d84027b9de074e533cdda10529f803aea
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-07-08 22:28:05 +0500

    LC-projection-area-of-3d-shapes [Runtime: N/A; Memory: N/A]

---
+++ array/projection-area-of-3d-shapes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int projectionArea(vector<vector<int>>& grid) {
+        int result = 0;
+        int mX = 0, mY = 0;
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++)
+        {
+            count = grid[i].size();
+            for (int j = 0; j < grid[i].size(); j++)
+            {
+                if (grid[i][j] > 0)
+                    result++;
+                if (mX < grid[i][j])
+                    mX = grid[i][j];
+            }
+            result += mX;
+        }
+        for (int j = 0; j < count; j++)
+        {
+            for (int i = 0; i < grid.size(); i++)
+                if (mY < grid[i][j])
+                    mY = grid[i][j];
+            result += mY;
+        }
+        return result;
+    }
+};
