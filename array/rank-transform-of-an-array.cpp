ec2ebc276e2728f38184acc64a6f84a22ee6e0e2
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-07-12 16:52:17 +0500

    LC-rank-transform-of-an-array [Runtime: N/A; Memory: N/A]

--- array/rank-transform-of-an-array.cpp
+++ array/rank-transform-of-an-array.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        int count = arr.size();
-        vector<int> sort_arr = arr;
-        sort(sort_arr.begin(),sort_arr.end());
+        // Store the rank for each number in arr
+        unordered_map<int, int> numToRank;
+        // Deduplicate and sort arr
+        set<int> nums(arr.begin(), arr.end());
         int rank = 1;
-        vector<int> rank_arr, result;
-        rank_arr.push_back(rank);
-        for (int i = 1; i < count; i++)
-        {
-            if (sort_arr[i] > sort_arr[i - 1])
-                rank++;
-            rank_arr.push_back(rank);
+        for (auto num : nums) {
+            numToRank[num] = rank;
+            rank++;
         }
-        for (int i = 0; i < count; i++)
-        {
-            for (int j = 0; j < count; j++)
-            {
-                if (arr[i] == sort_arr[j])
-                {
-                    result.push_back(rank_arr[j]);
-                    break;
-                }
-            }
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] = numToRank[arr[i]];
         }
-        return result;
+        return arr;
     }
 };
