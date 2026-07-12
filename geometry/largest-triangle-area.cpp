8bd112b25cc8169e282f0abbe65d7941c38648e9
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-07-12 21:32:25 +0500

    LC-largest-triangle-area [Runtime: N/A; Memory: N/A]

--- geometry/largest-triangle-area.cpp
+++ geometry/largest-triangle-area.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     double largestTriangleArea(vector<vector<int>>& points) {
-        double xmin = points[0][0], xmax = points[0][0], 
-               ymin = points[0][1], ymax = points[0][1];
-        for (int i = 1; i < points.size(); i++)
-        {
-            xmin = points[i][0] < xmin ? points[i][0] : xmin;
-            xmax = points[i][0] > xmax ? points[i][0] : xmax;
-            ymin = points[i][1] < ymin ? points[i][1] : ymin;
-            ymax = points[i][1] > ymax ? points[i][1] : ymax;
+        double maxArea = 0;
+        int n = points.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    double x1 = points[i][0];
+                    double y1 = points[i][1];
+                    double x2 = points[j][0];
+                    double y2 = points[j][1];
+                    double x3 = points[k][0];
+                    double y3 = points[k][1];
+                    double currentArea =
+                        abs(0.5 *
+                            (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)));
+                    maxArea = max(maxArea, currentArea);
+                }
+            }
         }
-        return ((xmax - xmin) * (ymax - ymin)) * 0.5;
+        return maxArea;
     }
 };
