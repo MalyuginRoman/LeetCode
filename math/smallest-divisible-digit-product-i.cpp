81b2f02c8d138e2c6d6b8aad4580961f0340e5e4
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-08-06 18:46:01 +0500

    LC-smallest-divisible-digit-product-i [Runtime: N/A; Memory: N/A]

---
+++ math/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        int result;
+        bool isCalc = false;
+        while (!isCalc)
+        {
+            int val = n >= 100 ? n/100 * (n%100)/10 * (n%100)%10 : (n%100)/10 * (n%100)%10;
+            if (val%t == 0)
+            {
+                result = n;
+                isCalc = true;
+            }
+            else
+                n++;
+        }
+        return result;
+    }
+};
