a4396e3014db4446e44300da21410eddfb80ded3
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2026-08-11 18:50:08 +0500

    LC-smallest-missing-integer-greater-than-sequential-prefix-sum [Runtime: N/A; Memory: N/A]

---
+++ array/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        auto max_it = std::max_element(nums.begin(), nums.end());
+        int max_val = *max_it;
+        int max_sum = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1] + 1)
+                max_sum += nums[i - 1];
+            else
+                break;
+        }
+        int result = max_val > max_sum ? max_val : max_sum;
+        return result + 1;
+    }
+};
